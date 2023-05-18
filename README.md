1d45a948703cf2a3f54ff3842656d69eca2be907
Jake Cyr <cyrjake@gmail.com>
2023-05-17 22:36:13 -0400

    Add basic app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# sms-gpt
-Service to SMS Chat with OpenAI GPT
+# SMS GPT
+
+A service to SMS Chat with OpenAI GPT.
+
+Before deploying to server, update the requirements.txt file with:
+
+```bash
+poetry export --without-hashes > requirements.txt 
+```
---
+++ sms_gpt/main.py
@@ -0,0 +1,50 @@
+from typing import Any
+from flask import Flask, abort, request
+from twilio.twiml.messaging_response import MessagingResponse
+import openai
+import os
+
+app = Flask(__name__)
+
+OPEN_AI_KEY: str | None = os.environ.get("OPENAI_API_KEY")
+
+if not OPEN_AI_KEY:
+    raise ValueError("Missing OPENAI_API_KEY env variable")
+
+openai.api_key = OPEN_AI_KEY
+
+
+@app.route("/sms/respond", methods=["GET", "POST"])
+def sms_reply() -> str:
+    # Get the incoming text message
+    body: str | None = request.values.get("Body", None)
+
+    if body is None:
+        abort(404)
+
+    messaging_response = MessagingResponse()
+
+    completion: Any = openai.ChatCompletion.create(
+        messages=[{"role": "user", "content": body}],
+        model="gpt-3.5-turbo",
+        max_tokens=500,
+        temperature=0.7,
+    )
+
+    choices = completion["choices"]
+    first_choice = choices[0]
+    first_choice_text: str = (
+        first_choice["message"]["content"].replace("\n", " ").strip()
+    )
+
+    messaging_response.message(first_choice_text)
+
+    return str(messaging_response)
+
+
+def start() -> None:
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    start()
